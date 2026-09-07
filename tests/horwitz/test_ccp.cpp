2d42716e670ccb1891c74dba65c06887d186db7e
Jeremy Horwitz <jhmail@gmail.com>
2026-09-07 14:10:19 -0700

    test_ccp.cpp: put expected before actual in all EXPECT_EQ calls

--- tests/horwitz/test_ccp.cpp
+++ tests/horwitz/test_ccp.cpp
@@ -9,135 +9,135 @@ extern "C" {
 TEST(RgbToHsv, Black) {
     RGB rgb = {.r = 0, .g = 0, .b = 0};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 0);
-    EXPECT_EQ(hsv.s, 0);
-    EXPECT_EQ(hsv.v, 0);
+    EXPECT_EQ(0, hsv.h);
+    EXPECT_EQ(0, hsv.s);
+    EXPECT_EQ(0, hsv.v);
 }
 
 TEST(RgbToHsv, White) {
     RGB rgb = {.r = 255, .g = 255, .b = 255};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 0);
-    EXPECT_EQ(hsv.s, 0);
-    EXPECT_EQ(hsv.v, 255);
+    EXPECT_EQ(0,   hsv.h);
+    EXPECT_EQ(0,   hsv.s);
+    EXPECT_EQ(255, hsv.v);
 }
 
 TEST(RgbToHsv, Red) {
     RGB rgb = {.r = 255, .g = 0, .b = 0};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 0);
-    EXPECT_EQ(hsv.s, 255);
-    EXPECT_EQ(hsv.v, 255);
+    EXPECT_EQ(0,   hsv.h);
+    EXPECT_EQ(255, hsv.s);
+    EXPECT_EQ(255, hsv.v);
 }
 
 TEST(RgbToHsv, Green) {
     RGB rgb = {.r = 0, .g = 255, .b = 0};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 85);
-    EXPECT_EQ(hsv.s, 255);
-    EXPECT_EQ(hsv.v, 255);
+    EXPECT_EQ(85,  hsv.h);
+    EXPECT_EQ(255, hsv.s);
+    EXPECT_EQ(255, hsv.v);
 }
 
 TEST(RgbToHsv, Blue) {
     RGB rgb = {.r = 0, .g = 0, .b = 255};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 170);
-    EXPECT_EQ(hsv.s, 255);
-    EXPECT_EQ(hsv.v, 255);
+    EXPECT_EQ(170, hsv.h);
+    EXPECT_EQ(255, hsv.s);
+    EXPECT_EQ(255, hsv.v);
 }
 
 TEST(RgbToHsv, Yellow) {
     RGB rgb = {.r = 255, .g = 255, .b = 0};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 43);
-    EXPECT_EQ(hsv.s, 255);
-    EXPECT_EQ(hsv.v, 255);
+    EXPECT_EQ(43,  hsv.h);
+    EXPECT_EQ(255, hsv.s);
+    EXPECT_EQ(255, hsv.v);
 }
 
 TEST(RgbToHsv, Cyan) {
     RGB rgb = {.r = 0, .g = 255, .b = 255};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 128);
-    EXPECT_EQ(hsv.s, 255);
-    EXPECT_EQ(hsv.v, 255);
+    EXPECT_EQ(128, hsv.h);
+    EXPECT_EQ(255, hsv.s);
+    EXPECT_EQ(255, hsv.v);
 }
 
 TEST(RgbToHsv, Magenta) {
     RGB rgb = {.r = 255, .g = 0, .b = 255};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 213);
-    EXPECT_EQ(hsv.s, 255);
-    EXPECT_EQ(hsv.v, 255);
+    EXPECT_EQ(213, hsv.h);
+    EXPECT_EQ(255, hsv.s);
+    EXPECT_EQ(255, hsv.v);
 }
 
 TEST(RgbToHsv, DarkRed) {
     RGB rgb = {.r = 128, .g = 0, .b = 0};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 0);
-    EXPECT_EQ(hsv.s, 255);
-    EXPECT_EQ(hsv.v, 128);
+    EXPECT_EQ(0,   hsv.h);
+    EXPECT_EQ(255, hsv.s);
+    EXPECT_EQ(128, hsv.v);
 }
 
 TEST(RgbToHsv, Orange) {
     RGB rgb = {.r = 255, .g = 128, .b = 0};
     HSV hsv = rgb_to_hsv(rgb);
-    EXPECT_EQ(hsv.h, 21);
-    EXPECT_EQ(hsv.s, 255);
-    EXPECT_EQ(hsv.v, 255);
+    EXPECT_EQ(21,  hsv.h);
+    EXPECT_EQ(255, hsv.s);
+    EXPECT_EQ(255, hsv.v);
 }
 
 // addBounded
 
 TEST(AddBounded, NoOverflow) {
-    EXPECT_EQ(addBounded(100, 50), 150);
+    EXPECT_EQ(150, addBounded(100, 50));
 }
 
 TEST(AddBounded, ExactMax) {
-    EXPECT_EQ(addBounded(200, 55), 255);
+    EXPECT_EQ(255, addBounded(200, 55));
 }
 
 TEST(AddBounded, Overflow) {
-    EXPECT_EQ(addBounded(250, 10), 255);
+    EXPECT_EQ(255, addBounded(250, 10));
 }
 
 TEST(AddBounded, ZeroPlusZero) {
-    EXPECT_EQ(addBounded(0, 0), 0);
+    EXPECT_EQ(0, addBounded(0, 0));
 }
 
 // subtractBounded
 
 TEST(SubtractBounded, NoUnderflow) {
-    EXPECT_EQ(subtractBounded(100, 50), 50);
+    EXPECT_EQ(50, subtractBounded(100, 50));
 }
 
 TEST(SubtractBounded, ExactZero) {
-    EXPECT_EQ(subtractBounded(50, 50), 0);
+    EXPECT_EQ(0, subtractBounded(50, 50));
 }
 
 TEST(SubtractBounded, Underflow) {
-    EXPECT_EQ(subtractBounded(10, 20), 0);
+    EXPECT_EQ(0, subtractBounded(10, 20));
 }
 
 TEST(SubtractBounded, ZeroMinusZero) {
-    EXPECT_EQ(subtractBounded(0, 0), 0);
+    EXPECT_EQ(0, subtractBounded(0, 0));
 }
 
 // min / max
 
 TEST(Min, ReturnsSmaller) {
-    EXPECT_EQ(min(3, 7), 3);
-    EXPECT_EQ(min(7, 3), 3);
+    EXPECT_EQ(3, min(3, 7));
+    EXPECT_EQ(3, min(7, 3));
 }
 
 TEST(Min, Equal) {
-    EXPECT_EQ(min(5, 5), 5);
+    EXPECT_EQ(5, min(5, 5));
 }
 
 TEST(Max, ReturnsLarger) {
-    EXPECT_EQ(max(3, 7), 7);
-    EXPECT_EQ(max(7, 3), 7);
+    EXPECT_EQ(7, max(3, 7));
+    EXPECT_EQ(7, max(7, 3));
 }
 
 TEST(Max, Equal) {
-    EXPECT_EQ(max(5, 5), 5);
+    EXPECT_EQ(5, max(5, 5));
 }
