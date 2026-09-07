958e6d4062111c1486b687bc341acc52bf10fc64
Jeremy Horwitz <jhmail@gmail.com>
2026-09-07 01:18:03 -0700

    add missing files (meant for earlier commits)

---
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/horwitz/colorconst.h
@@ -0,0 +1,10 @@
+#ifndef COLORCONST_H_INCLUDED
+#define COLORCONST_H_INCLUDED
+
+#define HUE_STEPS     256 // number of distinct hue values; hues are integers in [0, HUE_STEPS)
+#define MAX_COMPONENT 255 // maximum value for any RGB or HSV component
+#define HUE_SEXTANTS    6 // number of sextants in the HSV color wheel
+// NB: HUE_STEPS and MAX_COMPONENT are plain integer literals; cast the divisor to double when used in
+//     floating-point expressions (e.g., round(i * HUE_STEPS / (double)n)) to avoid integer division
+
+#endif
---
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/horwitz/layout.h
@@ -0,0 +1,7 @@
+#ifndef LAYOUT_H_INCLUDED
+#define LAYOUT_H_INCLUDED
+
+// LED indices for the K3 Pro ANSI 84-key layout (see keyboards/keychron/k3_pro/ansi/rgb/rgb.c)
+#define LED_INDEX_END 73
+
+#endif
